ecec41aaa015de78a6fc08364317da8975a1261e
kelicom <kelicom@qq.com>
2018-07-02 15:15:51 +0800

    init

---
+++ start-debug.bat
@@ -0,0 +1,20 @@
+php start.php start
+rem start
+rem php start.php start for debug mode
+
+rem php start.php start -d for daemon mode
+
+rem stop
+rem php start.php stop
+
+rem satus
+rem php start.php status
+
+rem restart
+rem php start.php restart
+
+rem reload
+rem php start.php reload
+
+rem connections
+rem php start.php connections
